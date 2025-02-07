015026ae3026944555b322230f159f282d5a925c
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-02-06 22:06:22 -0300

    Terminando segunda tela

--- app/src/main/java/com/dias/p6_calculadoraimc/ResultadoActivity.kt
+++ app/src/main/java/com/dias/p6_calculadoraimc/ResultadoActivity.kt
@@ -1,33 +1,20 @@
 package com.dias.p6_calculadoraimc
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnCalcular: Button
-
+class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_resultado)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btnCalcular = findViewById(R.id.btn_calcular)
-        //Abrir nova tela
-        btnCalcular.setOnClickListener {
-            val intent = Intent(this, ResultadoActivity::class.java)
-
-            startActivity(intent)
-        }
-
     }
 }
