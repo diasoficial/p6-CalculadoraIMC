3c2144aa505e84bbc359ca92cd8dfaf03966d9d3
Gabriel Dias <gabriel.dias025@hotmail.com>
2025-02-06 21:48:15 -0300

    Novo repositório, projeto Calculadora IMC

---
+++ app/src/main/java/com/dias/p6_calculadoraimc/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dias.p6_calculadoraimc
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnCalcular = findViewById(R.id.btn_calcular)
+        //Abrir nova tela
+        btnCalcular.setOnClickListener {
+
+        }
+
+    }
+}
